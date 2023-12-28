7d34517d39cd8c124278566bcf505343455fa5bc
Daniel Fildán <fildand@centrum.cz>
2023-12-28 08:26:29 +0100

    Upgrade Universal Wrench

--- src/main/java/net/danygames2014/imposterblocks/init/ItemListener.java
+++ src/main/java/net/danygames2014/imposterblocks/init/ItemListener.java
@@ -3,20 +3,25 @@ package net.danygames2014.imposterblocks.init;
 import net.danygames2014.imposterblocks.ImposterBlocks;
 import net.danygames2014.imposterblocks.item.Wrench;
 import net.danygames2014.uniwrench.api.WrenchMode;
+import net.mine_diver.unsafeevents.Event;
 import net.mine_diver.unsafeevents.listener.EventListener;
+import net.mine_diver.unsafeevents.listener.ListenerPriority;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.item.Item;
 import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
+import net.modificationstation.stationapi.api.event.resource.language.TranslationInvalidationEvent;
+import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
+import net.modificationstation.stationapi.api.util.Namespace;
+import net.modificationstation.stationapi.api.util.Null;
 
 public class ItemListener {
+    @Entrypoint.Namespace
+    public static final Namespace MOD_ID = Null.get();
+
     public static Item wrench;
 
     @EventListener
     public void registerWrench(ItemRegistryEvent event) {
-        wrench = new Wrench(ImposterBlocks.MOD_ID.id("wrench")).setTranslationKey(ImposterBlocks.MOD_ID, "wrench");
-
-        Wrench cast_wrench = (Wrench) wrench;
-        cast_wrench.addWrenchMode(WrenchMode.MODE_ROTATE);
-        cast_wrench.addWrenchMode(WrenchModeListener.MODE_TEXTURE);
-        cast_wrench.addWrenchMode(WrenchModeListener.MODE_DEBUG);
+        wrench = new Wrench(MOD_ID.id("wrench")).setTranslationKey(MOD_ID, "wrench");
     }
 }
--- src/main/java/net/danygames2014/imposterblocks/init/WrenchModeListener.java
+++ src/main/java/net/danygames2014/imposterblocks/init/WrenchModeListener.java
@@ -4,17 +4,27 @@ import net.danygames2014.imposterblocks.ImposterBlocks;
 import net.danygames2014.imposterblocks.item.Wrench;
 import net.danygames2014.uniwrench.api.WrenchMode;
 import net.danygames2014.uniwrench.api.event.UniversalWrenchModeEvent;
+import net.danygames2014.uniwrench.api.event.WrenchModeRegistryEvent;
 import net.mine_diver.unsafeevents.listener.EventListener;
+import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
+import net.modificationstation.stationapi.api.util.Namespace;
+import net.modificationstation.stationapi.api.util.Null;
 
 public class WrenchModeListener {
+    @Entrypoint.Namespace
+    public static final Namespace MOD_ID = Null.get();
+
     public static WrenchMode MODE_TEXTURE;
     public static WrenchMode MODE_DEBUG;
 
     @EventListener
-    public void registerWrenchMode(UniversalWrenchModeEvent event) {
-        MODE_TEXTURE = new WrenchMode(ImposterBlocks.MOD_ID.id("texture"));
-        MODE_DEBUG = new WrenchMode(ImposterBlocks.MOD_ID.id("debug"));
+    public void registerWrenchMode(WrenchModeRegistryEvent event) {
+        MODE_TEXTURE = new WrenchMode(MOD_ID.id("texture"));
+        MODE_DEBUG = new WrenchMode(MOD_ID.id("debug"));
+    }
 
+    @EventListener
+    public void addUniversalWrenchMode(UniversalWrenchModeEvent event){
         event.wrench.addWrenchMode(MODE_TEXTURE);
     }
 }
