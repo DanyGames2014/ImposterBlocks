9137900a2fadacef77a40c563d2fa96462126f8f
Daniel Fildán <fildand@centrum.cz>
2023-12-28 08:26:06 +0100

    Cleanup unused stuff

--- src/main/java/net/danygames2014/imposterblocks/ImposterBlocks.java
+++ src/main/java/net/danygames2014/imposterblocks/ImposterBlocks.java
@@ -1,6 +1,7 @@
 package net.danygames2014.imposterblocks;
 
 import jdk.jfr.Name;
+import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
@@ -12,9 +13,5 @@ import net.modificationstation.stationapi.api.util.Namespace;
 import net.modificationstation.stationapi.api.util.Null;
 
 public class ImposterBlocks {
-    @Entrypoint.Namespace
-    public static final Namespace MOD_ID = Null.get();
-
-    public static TagKey<Block> camoBlockTag = TagKey.of(BlockRegistry.INSTANCE.getKey(), Identifier.of("imposterblocks:camo_blocks"));
     public static TagKey<Item> camoBlockItemTag = TagKey.of(ItemRegistry.INSTANCE.getKey(), Identifier.of("imposterblocks:camo_blocks"));
 }
